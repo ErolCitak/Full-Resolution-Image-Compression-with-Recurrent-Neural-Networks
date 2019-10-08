d6e5184a6165911c8770fc1421873437f7a74696
chipyaya <b03902024@ntu.edu.tw>
2019-10-08 17:44:57 -0500

    encode iteratively; has hidden states passing issue; remove without pack version

--- project/__init__.py
+++ project/__init__.py
@@ -5,9 +5,49 @@ from torchvision.transforms import Resize, ToTensor, ToPILImage
 from PIL import Image
 
 
-use_packed = True
+# Binary output channel dimension
 b_outchannel_dim = 125
-print(f"use_packed: {use_packed}")
+# Number of iterations for encoding
+n_iterations = 16
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+print(f"device: {device}")
+
+def get_hidden_layers(sample_x):
+    encoder_h1 = (
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(device),
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(device)
+    )
+    encoder_h2 = (
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(device),
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(device))
+    encoder_h3 = (
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(device),
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(device)
+    )
+
+    decoder_h1 = (
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(device),
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(device)
+    )
+    decoder_h2 = (
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(device),
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(device)
+    )
+    decoder_h3 = (
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(device),
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(device)
+    )
+    decoder_h4 = (
+        torch.zeros(sample_x.size(0), 128, 128, 128).to(device),
+        torch.zeros(sample_x.size(0), 128, 128, 128).to(device)
+    )
+
+    return encoder_h1, encoder_h2, encoder_h3, decoder_h1, decoder_h2, decoder_h3, decoder_h4
+
 
 def encode(img, bottleneck):
     """
@@ -17,25 +57,25 @@ def encode(img, bottleneck):
     return: a numpy array less <= bottleneck bytes
     """
     input_transform = transforms.Compose([Resize((256, 256)), ToTensor()])
-    x = input_transform(img).unsqueeze(0)
+    sample_x = input_transform(img).unsqueeze(0).to(device)
 
-    h1 = (
-        torch.zeros(1, 256, 64, 64),
-        torch.zeros(1, 256, 64, 64)
-    )
-    h2 = (
-        torch.zeros(1, 512, 32, 32),
-        torch.zeros(1, 512, 32, 32))
-    h3 = (
-        torch.zeros(1, 512, 16, 16),
-        torch.zeros(1, 512, 16, 16)
-    )
+    encoder_h1, encoder_h2, encoder_h3, decoder_h1, decoder_h2, decoder_h3, decoder_h4 = \
+        get_hidden_layers(sample_x)
 
-    codes = binarizer.forward(encoder(x, h1, h2, h3)[0]).detach().numpy()
-    codes = codes.astype(np.int8)
-    if use_packed:
-        codes = (codes + 1) // 2
-        codes = np.packbits(codes, axis=1)
+    residual = sample_x
+    for i in range(n_iterations):
+        x, encoder_h1, encoder_h2, encoder_h3 = encoder(
+            residual, encoder_h1, encoder_h2, encoder_h3)
+        x = binarizer(x)
+        if i != n_iterations - 1:
+            output, decoder_h1, decoder_h2, decoder_h3, decoder_h4 = decoder(
+                x, decoder_h1, decoder_h2, decoder_h3, decoder_h4)
+            residual = sample_x - output
+
+    x = x.to(torch.device('cpu'))
+    codes = x.detach().numpy().astype(np.int8)
+    codes = (codes + 1) // 2
+    codes = np.packbits(codes, axis=1)
     print(f"nbytes: {codes.nbytes}")
     return codes
     
@@ -46,29 +86,11 @@ def decode(x, bottleneck):
     bottleneck: an integer from {4096,16384,65536}
     return a 256x256 PIL Image
     """
-    if use_packed:
-        x = np.unpackbits(x, axis=1, count=-(8 - b_outchannel_dim % 8))
-        x = x.astype(np.float32)
-        x = x * 2 - 1
+    x = np.unpackbits(x, axis=1, count=-(8 - b_outchannel_dim % 8)).astype(np.float32)
+    x = x * 2 - 1
+    x = torch.Tensor(x).to(device)
 
-    h1 = (
-        torch.zeros(1, 512, 16, 16),
-        torch.zeros(1, 512, 16, 16)
-    )
-    h2 = (
-        torch.zeros(1, 512, 32, 32),
-        torch.zeros(1, 512, 32, 32)
-    )
-    h3 = (
-        torch.zeros(1, 256, 64, 64),
-        torch.zeros(1, 256, 64, 64)
-    )
-    h4 = (
-        torch.zeros(1, 128, 128, 128),
-        torch.zeros(1, 128, 128, 128)
-    )
-
-    output = decoder.forward(torch.Tensor(x), h1, h2, h3, h4)[0]
+    output = decoder.forward(x, decoder_h1, decoder_h2, decoder_h3, decoder_h4)[0]
     output = output.squeeze(0)
     output = output.detach().cpu().numpy().transpose((1,2,0))
     output = Image.fromarray(np.uint8(output * 255))
@@ -81,16 +103,27 @@ Loading in Model
 """
 from .models import Encoder, Binarizer, Decoder
 
-encoder = Encoder()
-binarizer = Binarizer()
-decoder = Decoder()
+encoder = Encoder().to(device)
+binarizer = Binarizer().to(device)
+decoder = Decoder().to(device)
+
+# Load model weights here
+model_name = 'crnn_sig_lr001_125'
+if torch.cuda.is_available():
+    encoder.load_state_dict(
+        torch.load('project/save/{}_e.pth'.format(model_name))['model_state_dict'])
+    binarizer.load_state_dict(
+        torch.load('project/save/{}_b.pth'.format(model_name))['model_state_dict'])
+    decoder.load_state_dict(
+        torch.load('project/save/{}_d.pth'.format(model_name))['model_state_dict'])
+else:
+    encoder.load_state_dict(
+        torch.load('project/save/{}_e.pth'.format(model_name), map_location='cpu')['model_state_dict'])
+    binarizer.load_state_dict(
+        torch.load('project/save/{}_b.pth'.format(model_name), map_location='cpu')['model_state_dict'])
+    decoder.load_state_dict(
+        torch.load('project/save/{}_d.pth'.format(model_name), map_location='cpu')['model_state_dict'])
 
 encoder.eval()
 binarizer.eval()
 decoder.eval()
-
-# Load model weights here
-model_name = 'crnn_sig_lr001_125'
-encoder.load_state_dict(torch.load('project/save/{model_name}_e.pth'.format(model_name=model_name), map_location='cpu')['model_state_dict'])
-binarizer.load_state_dict(torch.load('project/save/{model_name}_b.pth'.format(model_name=model_name), map_location='cpu')['model_state_dict'])
-decoder.load_state_dict(torch.load('project/save/{model_name}_d.pth'.format(model_name=model_name), map_location='cpu')['model_state_dict'])
