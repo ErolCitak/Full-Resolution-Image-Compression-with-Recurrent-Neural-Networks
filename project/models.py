Fixed bug in residual calculation, uses sigmoid instead of tanh

project/models.py
@@ -121,7 +121,7 @@ class Encoder(nn.Module):
 
 
 class Binarizer(nn.Module):
-    def __init__(self, in_channels=512, out_channels=25):
+    def __init__(self, in_channels=512, out_channels=32):
         super(Binarizer, self).__init__()
         self.Conv = nn.Conv2d(
             in_channels=in_channels,
@@ -144,9 +144,10 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
 
         self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
 
         self.conv1 = nn.Conv2d(
-            in_channels=25,
+            in_channels=32,
             out_channels=512,
             kernel_size=1,
             stride=1,
@@ -220,5 +221,7 @@ class Decoder(nn.Module):
         x = hidden4[0]
         x = F.pixel_shuffle(x, 2)
 
-        out = self.tanh(self.conv2(x))/2
+        #out = self.tanh(self.conv2(x))
+        #out = (out+1)/2
+        out = self.sigmoid(self.conv2(x))
         return out, hidden1, hidden2, hidden3, hidden4

project/train.py
@@ -13,14 +13,59 @@ from models import Encoder, Decoder, Binarizer
 def img_normalize(imgs):
     return (imgs+1.0)/2
 
+def get_hidden_layers(args, sample_x):
+    encoder_h1 = (
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device),
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device)
+    )
+    encoder_h2 = (
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device),
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device))
+    encoder_h3 = (
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device),
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device)
+    )
 
-def save_models(args, encoder, binarizer, decoder):
-    torch.save(encoder.state_dict,
-               'save/{model_name}_e'.format(model_name=args.model_name))
-    torch.save(binarizer.state_dict,
-               'save/{model_name}_b'.format(model_name=args.model_name))
-    torch.save(decoder.state_dict,
-               'save/{model_name}_d'.format(model_name=args.model_name))
+    decoder_h1 = (
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device),
+        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device)
+    )
+    decoder_h2 = (
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device),
+        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device)
+    )
+    decoder_h3 = (
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device),
+        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device)
+    )
+    decoder_h4 = (
+        torch.zeros(sample_x.size(0), 128, 128, 128).to(args.device),
+        torch.zeros(sample_x.size(0), 128, 128, 128).to(args.device)
+    )
+
+    return encoder_h1, encoder_h2, encoder_h3, decoder_h1, decoder_h2, decoder_h3, decoder_h4
+
+def save_models(args, encoder, binarizer, decoder, epoch, enc_optimizer, dec_optimizer, binarizer_optimizer, loss):
+    path = "save/"
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': encoder.state_dict(),
+            'optimizer_state_dict': enc_optimizer.state_dict(),
+            'loss': loss
+            }, path + args.model_name+f"_e.pth")
+
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': binarizer.state_dict(),
+            'optimizer_state_dict': binarizer_optimizer.state_dict(),
+            'loss': loss
+            }, path + args.model_name+f"_b.pth")
+    torch.save({
+            'epoch': epoch,
+            'model_state_dict': decoder.state_dict(),
+            'optimizer_state_dict': dec_optimizer.state_dict(),
+            'loss': loss
+            }, path + args.model_name+f"_d.pth")
 
 
 def train(train_params, args, train_loader, val_loader):
@@ -50,39 +95,13 @@ def train(train_params, args, train_loader, val_loader):
     for epoch in range(train_params['epochs']):
         print('== Epoch:', epoch)
         epoch_loss = 0
+        curr_loss = 0
         for batch_idx, (sample_x, sample_y) in enumerate(train_loader):
             #print(f"batch_idx: {batch_idx}")
             sample_x = sample_x.to(args.device)
             sample_y = sample_y.to(args.device)
 
-            encoder_h1 = (
-                torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device),
-                torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device)
-            )
-            encoder_h2 = (
-                torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device),
-                torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device))
-            encoder_h3 = (
-                torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device),
-                torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device)
-            )
-
-            decoder_h1 = (
-                torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device),
-                torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device)
-            )
-            decoder_h2 = (
-                torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device),
-                torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device)
-            )
-            decoder_h3 = (
-                torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device),
-                torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device)
-            )
-            decoder_h4 = (
-                torch.zeros(sample_x.size(0), 128, 128, 128).to(args.device),
-                torch.zeros(sample_x.size(0), 128, 128, 128).to(args.device)
-            )
+            encoder_h1, encoder_h2, encoder_h3, decoder_h1, decoder_h2, decoder_h3, decoder_h4 = get_hidden_layers(args, sample_x)
 
             losses = []
             #losses = 0
@@ -103,7 +122,8 @@ def train(train_params, args, train_loader, val_loader):
                     x, decoder_h1, decoder_h2, decoder_h3, decoder_h4)
                 # print('output:', output.shape)
 
-                residual = residual - output
+                residual = sample_x - output
+                #residual = residual - output
                 #losses += residual.abs().mean()
                 losses.append(residual.abs().mean())
 
@@ -118,8 +138,10 @@ def train(train_params, args, train_loader, val_loader):
             if batch_idx % log_interval == 0:
                 idx = epoch * int(len(train_loader.dataset) / batch_size) + batch_idx
                 writer.add_scalar('loss', loss.item(), idx)
-                writer.add_image('input_img', img_normalize(sample_x[0]), idx)
-                writer.add_image('recon_img', img_normalize(sample_y[0]), idx)
+                writer.add_image('input_img', sample_x[0], idx)
+                writer.add_image('recon_img', output[0], idx)
+                #writer.add_image('recon_img', img_normalize(output[0]), idx)
+                curr_loss = 0
 
             #if batch_idx % val_interval == 0 and batch_idx != 0:
             if batch_idx % val_interval == 0 and train_params['validate']:
@@ -127,35 +149,7 @@ def train(train_params, args, train_loader, val_loader):
                 for batch_idx, (sample_x, sample_y) in enumerate(val_loader):
                     sample_x = sample_x.to(args.device)
                     sample_y = sample_y.to(args.device)
-
-                    encoder_h1 = (
-                        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device),
-                        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device)
-                    )
-                    encoder_h2 = (
-                        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device),
-                        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device))
-                    encoder_h3 = (
-                        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device),
-                        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device)
-                    )
-
-                    decoder_h1 = (
-                        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device),
-                        torch.zeros(sample_x.size(0), 512, 16, 16).to(args.device)
-                    )
-                    decoder_h2 = (
-                        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device),
-                        torch.zeros(sample_x.size(0), 512, 32, 32).to(args.device)
-                    )
-                    decoder_h3 = (
-                        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device),
-                        torch.zeros(sample_x.size(0), 256, 64, 64).to(args.device)
-                    )
-                    decoder_h4 = (
-                        torch.zeros(sample_x.size(0), 128, 128, 128).to(args.device),
-                        torch.zeros(sample_x.size(0), 128, 128, 128).to(args.device)
-                    )
+                    encoder_h1, encoder_h2, encoder_h3, decoder_h1, decoder_h2, decoder_h3, decoder_h4 = get_hidden_layers(args, sample_x)
                     x, encoder_h1, encoder_h2, encoder_h3 = encoder(
                         sample_x, encoder_h1, encoder_h2, encoder_h3)
                     x = binarizer(x)
@@ -171,7 +165,15 @@ def train(train_params, args, train_loader, val_loader):
                     best_encoder = copy.deepcopy(encoder)
                     best_binarizer = copy.deepcopy(binarizer)
                     best_decoder = copy.deepcopy(decoder)
-                    save_models(args, best_encoder, best_binarizer, best_decoder)
+                    save_models(args,
+                        encoder,
+                        binarizer,
+                        decoder,
+                        epoch,
+                        enc_optimizer,
+                        dec_optimizer,
+                        binarizer_optimizer,
+                        loss)
                     #save_models(args, encoder, binarizer, decoder)
                     print('Improved: current best_loss on val:{}'.format(best_loss))
                     patience = full_patience
@@ -179,12 +181,21 @@ def train(train_params, args, train_loader, val_loader):
                     patience -= 1
                     print('patience', patience)
                     if patience == 0:
-                        save_models(args, best_encoder, best_binarizer, best_decoder)
+                        save_models(args,
+                            best_encoder,
+                            best_binarizer,
+                            best_decoder,
+                            epoch,
+                            enc_optimizer,
+                            dec_optimizer,
+                            binarizer_optimizer,
+                            loss)
                         #save_models(args, encoder, binarizer, decoder)
                         print('Early Stopped: Best L1 loss on val:{}'.format(best_loss))
                         writer.close()
                         return
         print(f"epoch loss: {epoch_loss}")
+        writer.add_scalar('epoch loss', epoch_loss, epoch)
 
     print('Finished: Best L1 loss on val:{}'.format(best_loss))
     writer.close()